1c688cb98a5dd0dfae025afaafdf8e621526b369
Maksim Ramanau <MRamanau@ibagroup.eu>
2022-05-26 23:24:56 +0300

    Implement design for 2/5 pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,61 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewChild,
+  ViewContainerRef
+} from '@angular/core';
+import {MainComponent} from "./main/main.component";
+import {SymptomsComponent} from "./symptoms/symptoms.component";
+import {NgxSpinnerService} from "ngx-spinner";
+import {StatsComponent} from "./stats/stats.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit, AfterViewInit{
+  constructor(public viewContainerRef: ViewContainerRef,  public spinner: NgxSpinnerService, private cd: ChangeDetectorRef) {
+
+  }
+
+  activeMenu: string = 'main';
+  @ViewChild('placeholder', {read: ViewContainerRef, static: true}) placeholder!: ViewContainerRef
+
+  ngAfterViewInit(): void {
+    this.cd.detectChanges();
+    this.placeholder.clear();
+    this.placeholder.createComponent(MainComponent);
+  }
+
+  ngOnInit(): void {
+  }
+
+  async onMenuLinkClicked(id: string): Promise<void> {
+    await this.spinner.show();
+    this.placeholder.clear();
+
+    switch (id) {
+      case 'main':
+        this.placeholder.createComponent(MainComponent)
+        this.activeMenu = 'main'
+        break
+      case 'symptoms':
+        this.placeholder.createComponent(SymptomsComponent)
+        this.activeMenu = 'symptoms'
+        break
+      case 'stats':
+        this.placeholder.createComponent(StatsComponent)
+        this.activeMenu = 'stats'
+        break
+    }
+
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 1000)
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SymptomsComponent } from './symptoms/symptoms.component';
+import { MainComponent } from './main/main.component';
+import {NgxSpinnerModule} from "ngx-spinner";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SymptomsComponent,
+    MainComponent,
+    StatsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    NgxSpinnerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
